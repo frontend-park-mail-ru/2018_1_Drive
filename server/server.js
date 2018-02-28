14d97131085790d2557d05832a3a8ebd9e7e6e58
eugenmorozov <eugen.letterbox@mail.ru>
2018-02-28 22:47:24 +0300

    Ненастоящий коммит

--- server/server.js
+++ server/server.js
@@ -6,20 +6,22 @@ const fs = require('fs');
 const server = http.createServer(function(request,responce){
 	console.log(request.url, request.method);
 
-	if(request.url == "/css/styles.css"){
-		const style = fs.readFileSync('./css/styles.css','utf8');
+	if(request.url == "/static/css/styles.css"){
+		const style = fs.readFileSync('../public/static/css/styles.css','utf8');
 		responce.end(style);
 	}
-	else if (request.url == "/js/script.js"){
-		const text = fs.readFileSync('./js/script.js','utf8');
+	else if (request.url == "/index.js"){
+		const text = fs.readFileSync('../public/index.js','utf8');
 		responce.end(text);
 	}
 	else{
-		const text = fs.readFileSync('index.html','utf8');
+		const text = fs.readFileSync('../public/index.html','utf8');
 		responce.end(text);
 	}
 
 });
 
-server.listen(3000);
+
+
+server.listen(process.env.PORT||3000);
 console.log('Server startred');
