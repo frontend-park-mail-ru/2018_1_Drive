39fc8eaf9056007d0c8fdf3029f6252e2bb8a70d
eugenmorozov <eugen.letterbox@mail.ru>
2018-03-02 19:13:45 +0300

    +Сервер отдает каринки

--- server/server.js
+++ server/server.js
@@ -1,27 +1,33 @@
 'use strict';
 
-const http = require('http');
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-const server = http.createServer(function(request,responce){
-	console.log(request.url, request.method);
 
-	if(request.url == "/static/css/styles.css"){
-		const style = fs.readFileSync('./public/static/css/styles.css','utf8');
-		responce.end(style);
-	}
-	else if (request.url == "/index.js"){
-		const text = fs.readFileSync('./public/index.js','utf8');
-		responce.end(text);
-	}
-	else{
-		const text = fs.readFileSync('./public/index.html','utf8');
-		responce.end(text);
-	}
+const serverConfig = {
+    folderWithStatic: '../public',
+    defaultPort: 3000
+};
+
+
+const server = http.createServer((request, response)=> {
+
+    if (request.url === '/') {
+        request.url = '/index.html';
+    }
+
+    fs.readFile(path.join(__dirname, serverConfig.folderWithStatic, request.url), (error, data) => {
+        if (error) {
+            response.write('Page that you are looking for doesn\'t exist');
+            return;
+        }
+        response.write(data);
+        response.end();
+    });
 
 });
 
 
-
-server.listen(process.env.PORT||3000);
-console.log('Server startred');
+server.listen(process.env.PORT || serverConfig.defaultPort);
+console.log('Server started!');
