5d29b32d13c46a81f90087524a279af1255acf9c
eugenmorozov <eugen.letterbox@mail.ru>
2018-03-02 18:46:09 +0300

    +Исправлен баг с сервером

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ const server = http.createServer(function(request,responce){
 	console.log(request.url, request.method);
 
 	if(request.url == "/static/css/styles.css"){
-		const style = fs.readFileSync('../public/static/css/styles.css','utf8');
+		const style = fs.readFileSync('./public/static/css/styles.css','utf8');
 		responce.end(style);
 	}
 	else if (request.url == "/index.js"){
-		const text = fs.readFileSync('../public/index.js','utf8');
+		const text = fs.readFileSync('./public/index.js','utf8');
 		responce.end(text);
 	}
 	else{
-		const text = fs.readFileSync('../public/index.html','utf8');
+		const text = fs.readFileSync('./public/index.html','utf8');
 		responce.end(text);
 	}
 
