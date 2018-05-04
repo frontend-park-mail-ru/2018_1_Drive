1e878529e45fc0460b60f7dfcd0fc33305b11ea4
eugenmorozov <eugen.letterbox@mail.ru>
2018-05-04 17:58:24 +0300

    fixed commits

--- public/js/modules/game/core/offline.js
+++ public/js/modules/game/core/offline.js
@@ -50,7 +50,11 @@ export class OfflineGame extends GameCore {
                 this.bus.emit(events.THEME_SELECTED, themeButton.innerHTML);
             });
         }
-
+        navigator.serviceWorker.register('/appCache.js').then(function (registration) {
+            console.log('ServiceWorker registration', registration);
+        }).catch(function (err) {
+            throw new Error('ServiceWorker error: ' + err);
+        });
         this.state = {};
         this.gameloop = this.gameloop.bind(this);
         this.gameloopRequestId = null;
