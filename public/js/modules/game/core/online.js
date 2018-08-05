3f417dd1039e20faafd9b9a7f3df90f8a0e67055
andreynnt <andrejbabkov@gmail.com>
2018-08-05 13:42:00 +0300

    add new singleplayer

--- public/js/modules/game/core/online.js
+++ public/js/modules/game/core/online.js
@@ -2,9 +2,10 @@ import {BaseComponent} from '../../../blocks/baseComponent';
 import {events} from './events';
 import {GameCore} from './index';
 import * as busSingleton from '../../bus';
-import {Timer} from './timer';
+import {Timer} from '../../../modules/game/core/timer';
 import GameSettings from '../game-settings';
 import GameHttp from '../game-http';
+import {multiPlayerEvents} from '../multiplayer/events';
 
 
 let questionsAndAnswers = {};
@@ -13,31 +14,41 @@ export class OnlineGame extends GameCore {
     constructor() {
         super();
         this.bus = busSingleton.getInstance();
+        this.themeBlock = new BaseComponent(document.querySelector('.js-themes'));
+        this.questionsBlock = new BaseComponent(document.querySelector('.js-questions'));
+        //this.resultMenu = new BaseComponent(document.querySelector('.result'));
+        //this.gameProgressBar = document.querySelector('.progress-theme_js');
+        //this.roundProgressBar  = document.querySelector('.progress-question_js');
 
-        this.themeMenu = new BaseComponent(document.querySelector('.themes'));
-        this.questionMenu = new BaseComponent(document.querySelector('.questions'));
-        this.resultMenu = new BaseComponent(document.querySelector('.result'));
-        this.gameProgressBar = document.querySelector('.progress-theme_js');
-        this.roundProgressBar  = document.querySelector('.progress-question_js');
+        this.question = document.querySelector('.js-main-question');
+        this.answerButtons = document.querySelectorAll('.js-answer-button');
+        this.themeButtons = document.querySelectorAll('.js-theme-button');
 
-        this.question = document.querySelector('.question_block');
-        this.answerButtons = document.querySelectorAll('.answers_js');
-        this.themeButtons = document.querySelectorAll('.themes_js');
-        this.resultButton = document.querySelector('.center-block-singleplayer');
-        this.endButton = document.querySelector('.endGame_js');
-        this.againButton = document.querySelector('.again_js');
-        this.timer = new Timer(document.querySelector('canvas'));
+        this.headerOpponent = document.querySelector('.header__opponent');
+        this.headerOpponent.innerHTML = 'Singleplayer';
 
-        this.questionMenu.hide();
-        this.themeMenu.hide();
-        this.resultMenu.hide();
+        this.headerRound = document.querySelector('.header__round');
+        this.headerTheme = document.querySelector('.header__theme');
+        this.headerHyphen = document.querySelector('.header__hyphen');
 
-        this.againButton.addEventListener('click', () => {
-            this.bus.emit(events.START_GAME);
-        });
-        this.endButton.addEventListener('click', () => {
-            this.bus.emit(events.FINISH_GAME);
-        });
+        //this.resultButton = document.querySelector('.center-block-singleplayer');
+        //this.endButton = document.querySelector('.endGame_js');
+        //this.againButton = document.querySelector('.again_js');
+        this.timerDiv = document.querySelector('.header__timer');
+        this.infoDiv = document.querySelector('.header__waiting-block');
+        this.roundTime = 13;
+        this.timer = this.initTimer(this.timerDiv, this.infoDiv);
+
+        this.questionsBlock.hide();
+        this.themeBlock.hide();
+        // this.resultMenu.hide();
+
+        // this.againButton.addEventListener('click', () => {
+        //     this.bus.emit(events.START_GAME);
+        // });
+        // this.endButton.addEventListener('click', () => {
+        //     this.bus.emit(events.FINISH_GAME);
+        // });
 
         let buttonNum = 1;
         for (let answerButton of this.answerButtons) {
@@ -61,12 +72,20 @@ export class OnlineGame extends GameCore {
             });
         }
         this.state = {};
-        this.gameloop = this.gameloop.bind(this);
-        this.gameloopRequestId = null;
-        this.lastFrame = 0;
     }
 
-    start() {//initial state здесь надо отрисовать менюшку и скрытые вопросы
+    initTimer(timerDiv, infoDiv) {
+        const onStart = () => {
+            timerDiv.innerHTML = `${this.roundTime}\'\'`;
+            infoDiv.innerHTML = 'Time left';
+        };
+        const onEnd = () => {
+            //this.bus.emit(events['TIME_OVER']);
+        };
+        return new Timer(timerDiv, onStart, onEnd);
+    }
+
+    start() {
         super.start();
         //в answers храним нули и единицы
         this.state = {
@@ -75,19 +94,16 @@ export class OnlineGame extends GameCore {
             currentRound: 1,
             currentQuestionNum: 0
         };
+        this.headerRound.innerHTML = 'Choose theme';
+        this.headerHyphen.innerHTML = '';
+        this.headerTheme.innerHTML = '';
         this.bus.emit(events.START_GAME);
-        //setTimeout(function () {
-        //    bus.emit(events.START_GAME, this.state);
-        //}.bind(this));
     }
 
-    gameloop(now) {
-        this.gameloopRequestId = requestAnimationFrame(this.gameloop);
-    }
 
     onGameStarted(evt) {
-        this.gameProgressBar.style.left = '-100%';
-        this.roundProgressBar.style.left = '-100%';
+        // this.gameProgressBar.style.left = '-100%';
+        // this.roundProgressBar.style.left = '-100%';
         this.state = {
             theme: {},
             answers: [],
@@ -95,10 +111,8 @@ export class OnlineGame extends GameCore {
             currentQuestionNum: 0,
             correctAnswersInRound: 0
         };
-        this.resultMenu.hide();
-        this.themeMenu.show();
-        this.lastFrame = performance.now();
-        this.gameloopRequestId = requestAnimationFrame(this.gameloop);
+        //this.resultMenu.hide();
+        this.themeBlock.show();
     }
 
     onSetStarted(evt) {
@@ -107,10 +121,13 @@ export class OnlineGame extends GameCore {
     }
 
     onRoundStarted(evt) {
-        let pb =  (- 100 + 33 * (this.state.answers.length / 3)) + '%';
-        this.gameProgressBar.style.left = pb;
+        //let pb =  (- 100 + 33 * (this.state.answers.length / 3)) + '%';
+        //this.gameProgressBar.style.left = pb;
         this.state.correctAnswersInRound = 0;
-        this.themeMenu.show();
+        this.headerRound.innerHTML = 'Choose theme';
+        this.headerHyphen.innerHTML = '';
+        this.headerTheme.innerHTML = '';
+        this.themeBlock.show();
     }
 
     async onThemeSelected(evt) {
@@ -122,28 +139,28 @@ export class OnlineGame extends GameCore {
             });
 
         this.state.theme = evt;
-        this.themeMenu.hide();
-        this.questionMenu.show();
-        //TODO: method requestSet(theme) -> question_set
+        this.themeBlock.hide();
+        this.questionsBlock.show();
         this.state.currentQuestionNum = 0;
+        this.headerTheme.innerHTML = evt;
+        this.headerHyphen.innerHTML = '-';
+        this.headerRound.innerHTML = `Round ${this.state.currentRound}/${GameSettings.numberOfSets}`;
         this.bus.emit(events.GAME_STATE_CHANGED, this.state);
     }
 
     onTimeOver(evt){
-        this.state.answers.push(0);
-        this.state.currentQuestionNum++;
-        this.bus.emit(events.GAME_STATE_CHANGED);
+        this.onAnswerSelected(-1);
     }
 
     onGameStateChanged(evt) {
-        let pb =  (- 100 + 33 * (this.state.answers.length % 3)) + '%';
-        this.roundProgressBar.style.left = pb;
+        //let pb =  (- 100 + 33 * (this.state.answers.length % 3)) + '%';
+        //this.roundProgressBar.style.left = pb;
         let i = this.state.currentQuestionNum;
         if (i === GameSettings.numberOfSets) {
             this.timer.stop();
             this.bus.emit(events.ROUND_FINISHED);
         } else {
-            this.timer.start((new Date).getTime());
+            this.timer.start(this.roundTime);
             this.question.innerHTML = questionsAndAnswers[i].question.question;
             this.state.questionId = questionsAndAnswers[i].question.id;
             let j = 0;
@@ -157,7 +174,7 @@ export class OnlineGame extends GameCore {
 
     async onAnswerSelected(buttonNum) {
         let answers = {};
-
+        this.timer.stop();
         await GameHttp.checkAnswer(this.state.questionId, buttonNum)
             .then((response) => {
                 answers = {
@@ -176,9 +193,8 @@ export class OnlineGame extends GameCore {
             })
             .catch((error) => console.log(error));
 
-        await this.resolveAfterXSeconds(1800, answers);
+        await this.resolveAfterXSeconds(1500, answers);
         this.takeOffAnimation();
-
         this.state.currentQuestionNum++;
         this.bus.emit(events.GAME_STATE_CHANGED);
     }
@@ -196,21 +212,21 @@ export class OnlineGame extends GameCore {
     animateAnswers(answers) {
         for (let answerButton of this.answerButtons) {
             if (answerButton.buttonNum === answers.correctAnswer) {
-                answerButton.classList.add('animation-green');
+                answerButton.classList.add('correct-answer');
             }
             if (answerButton.buttonNum === answers.myAnswer && (answers.myAnswer !== answers.correctAnswer)) {
-                answerButton.classList.add('animation-red');
+                answerButton.classList.add('wrong-answer');
             }
         }
     }
 
     takeOffAnimation() {
         for (let answerButton of this.answerButtons) {
-            if (answerButton.classList.contains('animation-green')) {
-                answerButton.classList.remove('animation-green');
+            if (answerButton.classList.contains('correct-answer')) {
+                answerButton.classList.remove('correct-answer');
             }
-            if (answerButton.classList.contains('animation-red')) {
-                answerButton.classList.remove('animation-red');
+            if (answerButton.classList.contains('wrong-answer')) {
+                answerButton.classList.remove('wrong-answer');
             }
             if (answerButton.classList.contains('scale-animation')) {
                 answerButton.removeChild(answerButton.firstChild);
@@ -221,8 +237,11 @@ export class OnlineGame extends GameCore {
 
 
     onRoundFinished(evt) {
-        this.roundProgressBar.style.left = '-100%';
-        this.questionMenu.hide();
+        //this.roundProgressBar.style.left = '-100%';
+        this.questionsBlock.hide();
+
+        this.timerDiv.innerHTML = '';
+        this.infoDiv.innerHTML = '';
 
         if (this.state.currentRound === GameSettings.numberOfSets) {
             this.bus.emit(events.SET_FINISHED);
@@ -233,7 +252,7 @@ export class OnlineGame extends GameCore {
     }
 
     onSetFinished(evt) {
-        this.resultMenu.show();
+        //this.resultMenu.show();
         let result  = 0;
         let counter = 0;
         let questionInRound = 1;
@@ -249,11 +268,10 @@ export class OnlineGame extends GameCore {
             }
             questionInRound++;
         }
-        this.resultButton.childNodes[0].innerHTML = 'Your result is ' + result + ' / 3';
+        //this.resultButton.childNodes[0].innerHTML = 'Your result is ' + result + ' / 3';
     }
 
     onGameFinished(evt) {
-        cancelAnimationFrame(this.gameloopRequestId);
         this.bus.emit('home');
     }
 
