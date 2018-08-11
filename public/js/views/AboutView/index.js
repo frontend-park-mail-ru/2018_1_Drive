8513ff249b1117bf953b38d1128a9b601966f0e2
andreynnt <andrejbabkov@gmail.com>
2018-08-11 12:04:56 +0300

    fix home button

--- public/js/views/AboutView/index.js
+++ public/js/views/AboutView/index.js
@@ -1,6 +1,9 @@
 import {View} from '../View/view';
 import {HomeButton} from '../../blocks/home-button/HomeButton';
+import * as UserSingleton from '../../services/user-singletone';
+import {ProfileBlock} from '../../blocks/profile-block/profile';
 const aboutViewTempalte = require('./about-view.pug');
+const ProfileBlockTemplate = require('../../blocks/profile-block/profile-block.pug');
 
 export class AboutView extends View {
     constructor() {
@@ -11,11 +14,18 @@ export class AboutView extends View {
         super.create();
         const homeButton = new HomeButton();
         homeButton.render(this.el);
+
+        const userSingleton = UserSingleton.getInstance();
+        const profile = new ProfileBlock(this.el, ProfileBlockTemplate);
+        profile.render(userSingleton.getUser());
         super.hide();
         return this;
     }
 
     show() {
+        const userSingleton = UserSingleton.getInstance();
+        const profile = new ProfileBlock(this.el, ProfileBlockTemplate);
+        profile.render(userSingleton.getUser());
         this.el.removeAttribute('hidden');
         this.active = true;
         return this;
